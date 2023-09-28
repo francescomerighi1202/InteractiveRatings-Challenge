93c9698e2cf8fa008517e7735be5f42736f3c575
francescomerighi1202 <francescomerighi61@gmail.com>
2023-09-29 00:18:06 +0200

    Bug fix

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Bottoni (1, 2, 3, 4, 5)
 let rating_btns = document.querySelectorAll('.point');
 
 rating_btns.forEach(btn => {
@@ -13,17 +14,23 @@ rating_btns.forEach(btn => {
     });
 })
 
+// Bottone submit
 let submit_btn = document.querySelector('.submit-btn');
 let recap = document.querySelector('.recap');
 
+// Main card e second card
 let card = document.querySelector('.card');
 let ty_card = document.querySelector('.card-thank-you');
 
 submit_btn.addEventListener('click', () => {
+    // Bottone selezionato
     let selected_btn = document.querySelector('.point.selected');
 
+    // Se esiste un bottone selezionato
     if (selected_btn) {
+        // Prende il valore dall'attributo data-score nel tag HTML
         let rating = selected_btn.getAttribute('data-score');
+        // Imposta il testo della second card con il valore e la fa apparire al posto della main card
         recap.textContent = "You selected " + rating + " out of 5";
         card.style.display = "none";
         ty_card.style.display = "block";
